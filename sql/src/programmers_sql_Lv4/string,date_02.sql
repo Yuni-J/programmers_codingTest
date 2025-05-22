a9d748368f0813dd730d5bb7a28abe205daadf21
Yuni-J <okchipo2@naver.com>
2025-05-22 21:33:42 +0900

    sql_25

---
+++ sql/src/programmers_sql_Lv4/string,date_02.sql
@@ -0,0 +1,18 @@
+/* 취소되지 않은 진료 예약 조회하기 */
+
+SELECT 
+    A.APNT_NO
+    , P.PT_NAME
+    , P.PT_NO
+    , A.MCDP_CD
+    , D.DR_NAME
+    , A.APNT_YMD
+FROM APPOINTMENT AS A
+JOIN DOCTOR AS D
+    ON A.MDDR_ID = D.DR_ID
+JOIN PATIENT AS P
+    ON A.PT_NO = P.PT_NO
+WHERE A.APNT_YMD LIKE '2022-04-13%'
+    AND A.APNT_CNCL_YN = 'N'
+    AND D.MCDP_CD = 'CS'
+ORDER BY A.APNT_YMD;

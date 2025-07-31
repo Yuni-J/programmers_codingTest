664cb400512f758eaa9315cb7a22e5108f93d1f5
Yuni-J <okchipo2@naver.com>
2025-07-31 22:31:22 +0900

    java_70

--- java/src/programmers01/Lv0_70.java
+++ java/src/programmers01/Lv0_70.java
@@ -1,5 +1,18 @@
 package programmers01;
 
 public class Lv0_70 {
-
+	// n보다 커질 때까지 더하기
+	
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        
+        for(int i = 0; i < numbers.length; i++){
+            answer += numbers[i];
+            if(answer > n){
+                return answer;
+            }
+        }
+        
+        return answer;
+    }
 }

9a4060d663b4225c73e96f9da08c60c6a6a54d7f
Yuni-J <okchipo2@naver.com>
2025-08-04 22:40:31 +0900

    java_72

---
+++ java/src/programmers01/Lv0_72.java
@@ -0,0 +1,21 @@
+package programmers01;
+
+public class Lv0_72 {
+	// 조건에 맞게 수열 변환하기 1
+	
+    public int[] solution(int[] arr) {
+        int[] answer = new int[arr.length];
+        
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] >= 50 && arr[i] % 2 == 0){
+                answer[i] = arr[i] / 2;
+            } else if(arr[i] < 50 && arr[i] % 2 == 1){
+                answer[i] = arr[i] * 2;
+            } else {
+                answer[i] = arr[i];
+            }
+        }
+        
+        return answer;
+    }
+}

ebe8bff3382d5d570031073139a0f18c0059a5f6
Yuni-J <okchipo2@naver.com>
2025-08-12 18:59:11 +0900

    java_77,78

---
+++ java/src/programmers01/Lv0_77.java
@@ -0,0 +1,10 @@
+package programmers01;
+
+public class Lv0_77 {
+	// 대문자로 바꾸기
+	
+    public String solution(String myString) {
+
+        return myString.toUpperCase();
+    }
+}
---
+++ java/src/programmers01/Lv0_78.java
@@ -0,0 +1,10 @@
+package programmers01;
+
+public class Lv0_78 {
+	// 소문자로 바꾸기
+	
+    public String solution(String myString) {
+        
+        return myString.toLowerCase();
+    }
+}

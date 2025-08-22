2479622ee56cff58063aa6fb6cbc77fd598001f5
Yuni-J <okchipo2@naver.com>
2025-08-22 20:11:50 +0900

    java_85

---
+++ java/src/programmers01/Lv0_85.java
@@ -0,0 +1,11 @@
+package programmers01;
+
+public class Lv0_85 {
+    // 공백으로 구분하기 1
+    
+    public String[] solution(String my_string) {
+        String[] answer = my_string.split(" ");
+        
+        return answer;
+    }
+}
